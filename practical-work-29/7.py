a2ef09f732af74888544b016969646121c51eb0d
fN9ne <seniorderzhavin@yandex.ru>
2023-01-18 18:36:53 +0300

    Adding an input format

--- practical-work-29/7.py
+++ practical-work-29/7.py
@@ -2,3 +2,7 @@ n = int(input('Введите количесвто книг: '))
 arr = [[x for x in input().split(': ')] for z in range(n)]
 d = {k: v for k, v in arr}
 print(d)
+# ВВОД:
+# Пушкин: Сказка о рыбаке и рыбке
+# Есенин: Письмо к женщине
+# Тургенев: Муму
